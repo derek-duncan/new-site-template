009ddac95cda5c2e41497b5036336aff910c7292
Derek Duncan <mail-spam@derekduncan.me>
2016-03-17 00:11:04 -0500

    got some backend started too

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ import json from 'koa-json';
 import compress from 'koa-compress';
 import path from 'path';
 import http from 'http';
+import fs from 'fs';
+import mongoose from 'mongoose';
 
 import config from './config.js';
 
@@ -17,19 +19,26 @@ import controller from './lib/controllers/controller.js';
 const app = new Koa();
 
 // Connect to mongoose
-// const connect = () => {
-//   const options = {
-//     server: {
-//       socketOptions: {
-//         keepAlive: 1
-//       }
-//     }
-//   };
-//   mongoose.connect(config.mongodb.url, options);
-// };
-// connect();
-// mongoose.connection.on('error', console.log);
-// mongoose.connection.on('disconnected', connect);
+const connect = () => {
+  const options = {
+    server: {
+      socketOptions: {
+        keepAlive: 1,
+      },
+    },
+  };
+  mongoose.connect(config.mongodb.url, options);
+};
+connect();
+mongoose.connection.on('error', console.log); // eslint-disable-line no-console
+mongoose.connection.on('disconnected', connect);
+
+// Initiate models
+const modelsDir = path.join(__dirname, 'lib', 'models');
+fs.readdirSync(modelsDir).forEach(modelFilename => {
+  const modelPath = path.join(modelsDir, modelFilename);
+  if (modelFilename.indexOf('.js') >= 0) require(modelPath);
+});
 
 // Setup Koa modules
 app.use(logger());
